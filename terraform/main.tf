cd10e9333178799352b20c3e5342e0b5816e9ffd
yelmach <yassineelmach0@gmail.com>
2026-08-17 10:48:33 +0100

    add Terraform backend configuration for S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version = ">= 1.15.8"
+}
+
 provider "aws" {
-  region = "eu-west-3"
+  profile = "helazzou"
+  region  = "eu-west-2"
 }
