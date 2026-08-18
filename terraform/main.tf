fa0cf45f9db78a15eedd62b598ba94c6234797c8
yelmach <yassineelmach0@gmail.com>
2026-08-18 12:19:19 +0100

    add networking module with VPC and subnet configurations, including outputs and variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
+module "networking" {
+  source = "./modules/networking"
 
-  required_version = ">= 1.15.8"
-}
-
-provider "aws" {
-  profile = "helazzou"
-  region  = "eu-west-2"
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  azs                  = var.azs
 }
