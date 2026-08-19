ba2bd6be12500c85462dce16fe49230e8c9ffe5a
helazzou <elazzouzi.hamza20@gmail.com>
2026-08-19 18:37:39 +0100

    add compute module configuration and project name variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,12 @@ module "networking" {
   private_subnet_cidrs = var.private_subnet_cidrs
   azs                  = var.azs
 }
+
+module "compute" {
+  source = "./modules/compute"
+
+  project_name       = var.project_name
+  private_subnet_ids = module.networking.private_subnet_ids
+  vpc_id             = module.networking.vpc_id
+  vpc_cidr           = var.vpc_cidr
+}
