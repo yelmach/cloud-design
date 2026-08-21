4cd8b859a23d4e41740a57625c72609e7dfa45eb
helazzou <elazzouzi.hamza20@gmail.com>
2026-08-21 12:39:34 +0100

    fix module source path and enhance security_identity module parameters

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,12 +17,15 @@ module "compute" {
   vpc_cidr           = var.vpc_cidr
 }
 module "security_identity" {
-  source = "./module/security_identity"
+  source = "./modules/security_identity"
 
-  project_name       = var.project_name
-  vpc_id             = module.networking.vpc_id
-  vpc_cidr           = var.vpc_cidr
-  private_subnet_ids = module.networking.private_subnet_ids
+  project_name          = var.project_name
+  vpc_id                = module.networking.vpc_id
+  vpc_cidr              = var.vpc_cidr
+  private_subnet_ids    = module.networking.private_subnet_ids
+  billing_db_password   = var.billing_db_password
+  inventory_db_password = var.inventory_db_password
+  rabbitmq_password     = var.rabbitmq_password
 }
 
 module "services" {
@@ -34,4 +37,16 @@ module "services" {
   private_subnet_ids = module.networking.private_subnet_ids
   ecs_cluster_id     = module.compute.ecs_cluster_arn
   dockerhub_username = var.dockerhub_username
+
+  cognito_user_pool_id   = module.security_identity.user_pool_id
+  cognito_client_id      = module.security_identity.user_pool_client_id
+  cognito_issuer_url     = module.security_identity.cognito_issuer_url
+  ecs_execution_role_arn = module.security_identity.ecs_execution_role_arn
+  ecs_task_role_arn      = module.security_identity.ecs_task_role_arn
+
+  alb_target_group_arn = module.security_identity.alb_target_group_arn
+
+  billing_db_password = module.security_identity.ssm_billing_db_password_arn
+  inventory_db_password = module.security_identity.ssm_inventory_db_password_arn
+  rabbitmq_password = module.security_identity.ssm_rabbitmq_password_arn
 }
